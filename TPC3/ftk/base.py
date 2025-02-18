4aa306927eab9619a1a10164386211a03d544053
Maria <miniimi2001@gmail.com>
2025-02-18 15:55:07 +0000

    melhorar isto

--- TPC3/ftk/base.py
+++ TPC3/ftk/base.py
@@ -1,13 +1,20 @@
 import re
 import jjcli
-
+from collections import Counter
 
 def lexer(txt):
     # FIXME - patterns stopwords lemas YET TO BE TREATED
     return re.findall(r'\w+(-\w+)*|[^\w\s]+',txt)
 
+def counter(tokens):
+    return Counter(*tokens) # FIXME
 
 def main():
     cl = jjcli.clfilter()
+    tokens = []
     for txt in cl.text():
-        print(lexer(txt))
+        t = lexer(txt)
+        print(t)
+        tokens.append(t)
+    c = counter(t)
+    print(c)
